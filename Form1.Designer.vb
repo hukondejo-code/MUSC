449fb193f478b8471b08046dd08923b720a6bc6e
hukondejo-code <hukondejo@hotmail.com>
2026-07-05 12:41:24 +0200

    Refactor Form1 by removing unused tabs and adding button
    Removed multiple unused tab pages and panels from the form designer, and added a shutdown button.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -29,39 +29,20 @@ Partial Class Form1
         ServerStartupToolStripMenuItem = New ToolStripMenuItem()
         SettingsToolStripMenuItem = New ToolStripMenuItem()
         ExitServerShutdownToolStripMenuItem = New ToolStripMenuItem()
+        BtnShutdownApps = New Button()
         TabControl1 = New TabControl()
         TabPage1 = New TabPage()
         Panel1 = New Panel()
-        TabPage2 = New TabPage()
-        Panel2 = New Panel()
-        TabPage3 = New TabPage()
-        Panel3 = New Panel()
-        TabPage4 = New TabPage()
-        Panel4 = New Panel()
-        TabPage5 = New TabPage()
-        Panel5 = New Panel()
-        TabPage6 = New TabPage()
-        Panel6 = New Panel()
-        TabPage7 = New TabPage()
-        Panel7 = New Panel()
-        TabPage8 = New TabPage()
-        Panel8 = New Panel()
         Panel9.SuspendLayout()
         MenuStrip1.SuspendLayout()
         TabControl1.SuspendLayout()
         TabPage1.SuspendLayout()
-        TabPage2.SuspendLayout()
-        TabPage3.SuspendLayout()
-        TabPage4.SuspendLayout()
-        TabPage5.SuspendLayout()
-        TabPage6.SuspendLayout()
-        TabPage7.SuspendLayout()
-        TabPage8.SuspendLayout()
         SuspendLayout()
         ' 
         ' Panel9
         ' 
         Panel9.Controls.Add(MenuStrip1)
+        Panel9.Controls.Add(BtnShutdownApps)
         Panel9.Dock = DockStyle.Top
         Panel9.Location = New Point(0, 0)
         Panel9.Name = "Panel9"
@@ -103,16 +84,19 @@ Partial Class Form1
         ExitServerShutdownToolStripMenuItem.Size = New Size(192, 22)
         ExitServerShutdownToolStripMenuItem.Text = "Exit (Server Shutdown)"
         ' 
+        ' BtnShutdownApps
+        ' 
+        BtnShutdownApps.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        BtnShutdownApps.Location = New Point(1000, 0)
+        BtnShutdownApps.Name = "BtnShutdownApps"
+        BtnShutdownApps.Size = New Size(110, 24)
+        BtnShutdownApps.TabIndex = 2
+        BtnShutdownApps.Text = "Shutdown Apps"
+        BtnShutdownApps.UseVisualStyleBackColor = True
+        ' 
         ' TabControl1
         ' 
         TabControl1.Controls.Add(TabPage1)
-        TabControl1.Controls.Add(TabPage2)
-        TabControl1.Controls.Add(TabPage3)
-        TabControl1.Controls.Add(TabPage4)
-        TabControl1.Controls.Add(TabPage5)
-        TabControl1.Controls.Add(TabPage6)
-        TabControl1.Controls.Add(TabPage7)
-        TabControl1.Controls.Add(TabPage8)
         TabControl1.Dock = DockStyle.Fill
         TabControl1.DrawMode = TabDrawMode.OwnerDrawFixed
         TabControl1.Location = New Point(0, 26)
@@ -140,136 +124,9 @@ Partial Class Form1
         Panel1.Size = New Size(1100, 552)
         Panel1.TabIndex = 0
         ' 
-        ' TabPage2
-        ' 
-        TabPage2.BackColor = Color.Transparent
-        TabPage2.Controls.Add(Panel2)
-        TabPage2.Location = New Point(4, 24)
-        TabPage2.Name = "TabPage2"
-        TabPage2.Padding = New Padding(3)
-        TabPage2.Size = New Size(1106, 558)
-        TabPage2.TabIndex = 1
-        TabPage2.Text = "NOT DEFINED"
-        ' 
-        ' Panel2
-        ' 
-        Panel2.Dock = DockStyle.Fill
-        Panel2.Location = New Point(3, 3)
-        Panel2.Name = "Panel2"
-        Panel2.Size = New Size(1100, 552)
-        Panel2.TabIndex = 0
-        ' 
-        ' TabPage3
-        ' 
-        TabPage3.BackColor = Color.Transparent
-        TabPage3.Controls.Add(Panel3)
-        TabPage3.Location = New Point(4, 24)
-        TabPage3.Name = "TabPage3"
-        TabPage3.Size = New Size(1106, 558)
-        TabPage3.TabIndex = 2
-        TabPage3.Text = "NOT DEFINED"
-        ' 
-        ' Panel3
-        ' 
-        Panel3.Dock = DockStyle.Fill
-        Panel3.Location = New Point(0, 0)
-        Panel3.Name = "Panel3"
-        Panel3.Size = New Size(1106, 558)
-        Panel3.TabIndex = 0
-        ' 
-        ' TabPage4
-        ' 
-        TabPage4.BackColor = Color.Transparent
-        TabPage4.Controls.Add(Panel4)
-        TabPage4.Location = New Point(4, 24)
-        TabPage4.Name = "TabPage4"
-        TabPage4.Size = New Size(1106, 558)
-        TabPage4.TabIndex = 3
-        TabPage4.Text = "NOT DEFINED"
-        ' 
-        ' Panel4
-        ' 
-        Panel4.Dock = DockStyle.Fill
-        Panel4.Location = New Point(0, 0)
-        Panel4.Name = "Panel4"
-        Panel4.Size = New Size(1106, 558)
-        Panel4.TabIndex = 0
-        ' 
-        ' TabPage5
-        ' 
-        TabPage5.BackColor = Color.Transparent
-        TabPage5.Controls.Add(Panel5)
-        TabPage5.Location = New Point(4, 24)
-        TabPage5.Name = "TabPage5"
-        TabPage5.Size = New Size(1106, 558)
-        TabPage5.TabIndex = 4
-        TabPage5.Text = "NOT DEFINED"
-        ' 
-        ' Panel5
-        ' 
-        Panel5.Dock = DockStyle.Fill
-        Panel5.Location = New Point(0, 0)
-        Panel5.Name = "Panel5"
-        Panel5.Size = New Size(1106, 558)
-        Panel5.TabIndex = 0
-        ' 
-        ' TabPage6
-        ' 
-        TabPage6.BackColor = Color.Transparent
-        TabPage6.Controls.Add(Panel6)
-        TabPage6.Location = New Point(4, 24)
-        TabPage6.Name = "TabPage6"
-        TabPage6.Size = New Size(1106, 558)
-        TabPage6.TabIndex = 5
-        TabPage6.Text = "NOT DEFINED"
-        ' 
-        ' Panel6
-        ' 
-        Panel6.Dock = DockStyle.Fill
-        Panel6.Location = New Point(0, 0)
-        Panel6.Name = "Panel6"
-        Panel6.Size = New Size(1106, 558)
-        Panel6.TabIndex = 0
-        ' 
-        ' TabPage7
-        ' 
-        TabPage7.BackColor = Color.Transparent
-        TabPage7.Controls.Add(Panel7)
-        TabPage7.Location = New Point(4, 24)
-        TabPage7.Name = "TabPage7"
-        TabPage7.Size = New Size(1106, 558)
-        TabPage7.TabIndex = 6
-        TabPage7.Text = "NOT DEFINED"
-        ' 
-        ' Panel7
-        ' 
-        Panel7.Dock = DockStyle.Fill
-        Panel7.Location = New Point(0, 0)
-        Panel7.Name = "Panel7"
-        Panel7.Size = New Size(1106, 558)
-        Panel7.TabIndex = 0
-        ' 
-        ' TabPage8
-        ' 
-        TabPage8.BackColor = Color.Transparent
-        TabPage8.Controls.Add(Panel8)
-        TabPage8.Location = New Point(4, 24)
-        TabPage8.Name = "TabPage8"
-        TabPage8.Size = New Size(1106, 558)
-        TabPage8.TabIndex = 7
-        TabPage8.Text = "NOT DEFINED"
-        ' 
-        ' Panel8
-        ' 
-        Panel8.Dock = DockStyle.Fill
-        Panel8.Location = New Point(0, 0)
-        Panel8.Name = "Panel8"
-        Panel8.Size = New Size(1106, 558)
-        Panel8.TabIndex = 0
-        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.WhiteSmoke
         ClientSize = New Size(1114, 612)
@@ -287,38 +144,19 @@ Partial Class Form1
         MenuStrip1.PerformLayout()
         TabControl1.ResumeLayout(False)
         TabPage1.ResumeLayout(False)
-        TabPage2.ResumeLayout(False)
-        TabPage3.ResumeLayout(False)
-        TabPage4.ResumeLayout(False)
-        TabPage5.ResumeLayout(False)
-        TabPage6.ResumeLayout(False)
-        TabPage7.ResumeLayout(False)
-        TabPage8.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents Panel9 As Panel
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
-    Friend WithEvents TabPage3 As TabPage
-    Friend WithEvents TabPage4 As TabPage
-    Friend WithEvents TabPage5 As TabPage
-    Friend WithEvents TabPage6 As TabPage
-    Friend WithEvents TabPage7 As TabPage
-    Friend WithEvents TabPage8 As TabPage
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents Panel2 As Panel
-    Friend WithEvents Panel3 As Panel
-    Friend WithEvents Panel4 As Panel
-    Friend WithEvents Panel5 As Panel
-    Friend WithEvents Panel6 As Panel
-    Friend WithEvents Panel7 As Panel
-    Friend WithEvents Panel8 As Panel
+    ' Only one tab + panel (dynamic tabs will be created at runtime)
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ServerStartupToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ExitServerShutdownToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BtnShutdownApps As Button
 
 End Class
